80de680157d747e10b9894663b7421362ce28494
Darrell Tunnell <darrell.tunnell@googlemail.com>
2021-06-02 14:23:21 +0100

    Clean up. Removed old expressions stuff.

--- src/Changify/Expressions/IFuncBoolBuilderInitial.cs
+++
@@ -1,9 +0,0 @@
-
-namespace System.Linq.Expressions
-{
-    public interface IFuncBoolBuilderInitial
-    {
-        IFuncBoolBuilder Initial(Expression<Func<bool>> expression);
-        IFuncBoolBuilder Initial(bool value);
-    }
-}
